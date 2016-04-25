43d5e0693914769f4add61a35348211d6ba5fcd4
Andreza Feu <andrezafeu@gmail.com>
2016-04-25 16:10:31 -0400

    Make normal crust and normal sauce the defaulds for the pizza

--- src/pizza.js
+++ src/pizza.js
@@ -1,14 +1,8 @@
-// // Write your Pizza Builder JavaScript in this file.
-// $('.pizza').toggleClass('pep one')
-// $('.pizza').toggleClass('pep two')
-// $('.pizza').toggleClass('green-pepper one')
-// $('.pizza').toggleClass('green-pepper two')
-// $('.pizza').toggleClass('mushroom one')
-// $('.pizza').toggleClass('mushroom two')
-
-
-
 $(document).on("ready", function () {
+
+	$(".sauce").removeClass('sauce-white');
+	$(".crust").removeClass('crust-gluten-free');
+
 
 	$(".btn-pepperonni").on("click", function () {
 		console.log('pepperonni');
@@ -24,14 +18,12 @@ $(document).on("ready", function () {
 	})
 
 	$(".btn-sauce").on("click", function () {
-		$(".sauce-white").toggle();
+		$(".sauce").toggleClass("sauce-white");
 	});
 
 	$(".btn-crust").on("click", function () {
 		$(".crust").toggleClass('crust-gluten-free');
 
 	});
-
-
 
 });
