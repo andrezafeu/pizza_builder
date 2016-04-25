733d8f0e78b9579f01e2d8b007542430e8e6c316
Andreza Feu <andrezafeu@gmail.com>
2016-04-25 18:22:35 -0400

    Show/hide price of individual toppings

--- src/pizza.js
+++ src/pizza.js
@@ -5,30 +5,38 @@ $(document).on("ready", function () {
 	$(".btn-sauce").toggleClass("active");
 	$(".btn-crust").toggleClass("active");
 
+	$(".js-whiteSauce").hide();
+	$(".js-glutenFree").hide();
+
 	$(".btn-pepperonni").on("click", function () {
 		console.log('pepperonni');
 		$(".pep").toggle();
 		$(".btn-pepperonni").toggleClass("active");
+		$(".js-pepperonni").toggle();
 	});
 
 	$(".btn-mushrooms").on("click", function () {
 		$(".cap, .stem").toggle();
 		$(".btn-mushrooms").toggleClass("active");
+		$(".js-mushrooms").toggle();
 	});
 
 	$(".btn-green-peppers").on("click", function () {
 		$(".green-pepper").toggle();
 		$(".btn-green-peppers").toggleClass("active");
+		$(".js-greenPeppers").toggle();
 	})
 
 	$(".btn-sauce").on("click", function () {
 		$(".sauce").toggleClass("sauce-white");
 		$(".btn-sauce").toggleClass("active");
+		$(".js-whiteSauce").toggle();
 	});
 
 	$(".btn-crust").on("click", function () {
 		$(".crust").toggleClass('crust-gluten-free');
 		$(".btn-crust").toggleClass("active");
+		$(".js-glutenFree").toggle();
 	});
 
 	$(".js-update-price").on("click", function () {
