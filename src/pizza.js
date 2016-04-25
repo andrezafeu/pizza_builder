d8d1a428a78b79da210e5bc2a6e4533da933544c
Andreza Feu <andrezafeu@gmail.com>
2016-04-25 18:02:48 -0400

    Change total price according to active butttons

--- src/pizza.js
+++ src/pizza.js
@@ -5,7 +5,6 @@ $(document).on("ready", function () {
 	$(".btn-sauce").toggleClass("active");
 	$(".btn-crust").toggleClass("active");
 
-
 	$(".btn-pepperonni").on("click", function () {
 		console.log('pepperonni');
 		$(".pep").toggle();
@@ -30,7 +29,30 @@ $(document).on("ready", function () {
 	$(".btn-crust").on("click", function () {
 		$(".crust").toggleClass('crust-gluten-free');
 		$(".btn-crust").toggleClass("active");
-
 	});
 
+	$(".js-update-price").on("click", function () {
+		console.log("update price")
+		
+		var pizzaPrice = 10;
+
+		var prices = {
+			".btn-pepperonni": 1,
+			".btn-green-peppers": 1,
+			".btn-mushrooms": 1,
+			".btn-sauce": 3,
+			".btn-crust": 5
+		}
+
+		for (selector in prices) {
+			thePrice = prices[selector]
+
+			if ($(selector).hasClass("active")) {
+				pizzaPrice += thePrice;
+			}
+		}
+
+		$ ("strong").text(pizzaPrice);
+	});
+	
 });
