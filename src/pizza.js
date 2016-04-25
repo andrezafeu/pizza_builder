d6b50e4f0d2504ec4df1a76d02e7b30d79cf1d8b
Andreza Feu <andrezafeu@gmail.com>
2016-04-25 16:25:44 -0400

    Change CSS of buttons to match the toppings that are on the pizza

--- src/pizza.js
+++ src/pizza.js
@@ -2,27 +2,34 @@ $(document).on("ready", function () {
 
 	$(".sauce").removeClass('sauce-white');
 	$(".crust").removeClass('crust-gluten-free');
+	$(".btn-sauce").toggleClass("active");
+	$(".btn-crust").toggleClass("active");
 
 
 	$(".btn-pepperonni").on("click", function () {
 		console.log('pepperonni');
 		$(".pep").toggle();
+		$(".btn-pepperonni").toggleClass("active");
 	});
 
 	$(".btn-mushrooms").on("click", function () {
 		$(".cap, .stem").toggle();
+		$(".btn-mushrooms").toggleClass("active");
 	});
 
 	$(".btn-green-peppers").on("click", function () {
 		$(".green-pepper").toggle();
+		$(".btn-green-peppers").toggleClass("active");
 	})
 
 	$(".btn-sauce").on("click", function () {
 		$(".sauce").toggleClass("sauce-white");
+		$(".btn-sauce").toggleClass("active");
 	});
 
 	$(".btn-crust").on("click", function () {
 		$(".crust").toggleClass('crust-gluten-free');
+		$(".btn-crust").toggleClass("active");
 
 	});
 
